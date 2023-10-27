6ea300b8d03e50800882e09044d74a22a58e26e7
athuler <athuler@uchicago.edu>
2023-10-27 13:04:03 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,420 @@
+<?php
+
+$GLOBALS["API_KEY"] = $_ENV["GOOGLE_API_KEY"];
+
+###### Create Video Tree #####
+require_once("video_data.php");
+$first_vid = "PmWQmZXYd74";
+
+
+###### Build Paths & Parents #####
+foreach($GLOBALS["data"] as $video_id => $video) {
+	$num_children = count($video->children);
+	
+	for($j = 0; $j < $num_children; $j ++) {
+		
+		
+		// Add Parent
+		$GLOBALS["data"][$video->children[$j]]->add_parent($video_id);
+		
+		// Add New Path(s)
+		$new_paths = [];
+		foreach($video->paths as $current_path) {
+			array_push($new_paths,
+				$current_path . $video->child_path[$j]
+			);
+		}
+		$GLOBALS["data"][$video->children[$j]]->add_path(array_unique($new_paths));
+		
+	}
+}
+
+
+
+###### Fetch Video Views ######
+// Process videos in batches of 50
+$all_ids = array_chunk(
+			array_keys($GLOBALS["data"]),
+			50);
+
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+foreach($all_ids as $id_list) {
+
+	curl_setopt_array($curl, [
+		CURLOPT_URL => "https://youtube.googleapis.com/youtube/v3/videos?part=statistics&id=" . implode(",", $id_list) . "&key=" . $GLOBALS["API_KEY"],
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => "",
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 30,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => "GET",
+		CURLOPT_HTTPHEADER => [],
+	]);
+	$response = json_decode(curl_exec($curl), true);
+	if (array_key_exists("items", $response)){
+		foreach($response["items"] as $vid) {
+			$GLOBALS["data"][$vid["id"]]->set_views($vid["statistics"]["viewCount"]);
+		}
+	} else {
+		echo("API ERROR");
+		echo("Num of Videos:" . sizeof($id_list));
+		var_dump($response);
+	}
+}
+//var_dump($GLOBALS["data"]);
+//exit();
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CGP Grey: Rock Paper Scissors - A Statistical Analysis</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+</head>
+<!--<body class="text-center container-fluid overflow-x-scroll">-->
+<body class="text-center overflow-x-auto">
+	<div class="row justify-content-center">
+		<div class="col-auto ">
+			<!-- Title -->
+			<h1 id="title">CGP Grey: Rock Paper Scissors</h1>
+			<h2>A Visual and Statistical Overview</h2>
+			<br/><br/>
+			
+			<!-- Visual Controls -->
+			<h3>Controls:</h3>
+			<input type="button" class="btn btn-primary" value="Show/Hide Children & Parents" onclick="toggle_family()"/>
+			<input type="button" class="btn btn-primary" value="Show/Hide All Paths" onclick="toggle_paths()"/>
+			
+			<!-- Statistics -->
+			<br/><br/>
+			<h3>Some statistics:</h3><p>
+			<b><?=count(array_keys($GLOBALS["data"]))?></b> videos<br/>
+			<b><?php
+				// Compute total video views
+				$total_views = 0;
+				foreach($GLOBALS["data"] as $vid){
+					$total_views += $vid->views;
+				}
+				echo(number_format($total_views));
+			?></b> total views
+			
+			</p><br/><br/>
+			
+			<!-- About -->
+			<h3>About this Project:</h3>
+			<p>Made by <a href="https://github.com/athuler" target="__blank">athuler</a> with much blood, sweats, and so many tears.</p>
+			<p>Bug? Something wrong? <a href="https://github.com/athuler/CgpGreyRockPaperScissors/issues" target="__blank">Report it!</a></p>
+			
+			<div class="row justify-content-center">
+			<a href="https://github.com/athuler/CgpGreyRockPaperScissors" target="__blank" class="col-auto"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+			<path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" stroke="black"/>
+			</svg></a>
+			
+			<a href="https://github.com/sponsors/athuler" target="__blank" class="col-auto"><img src="https://img.shields.io/static/v1?label=Sponsor&message=%E2%9D%A4&logo=GitHub&color=%23fe8e86"/></a>
+			</div>
+		</div>
+	</div>
+	<br/>
+	<div id="svgContainer" style="margin: 0px 0px;">
+		<svg id="svg1" width="0" height="0" >
+			<?php
+			###### Add Arrows #####
+			foreach($GLOBALS["data"] as $video_id => $video) {
+				$num_children = count($video->children);
+				for($j = 0; $j < $num_children; $j ++) {
+					if($video->child_path[$j] == "W") {
+						$color = "green";
+					} else if($video->child_path[$j] == "L") {
+						$color = "red";
+					} else {
+						$color = "grey";
+					}
+				?>
+				<path
+					id="<?=$video_id . "-" . $video->children[$j]?>"
+					d="M0 0"		 
+					stroke="<?=$color?>" 
+					fill="none"
+					stroke-width="<?=$video->views/$GLOBALS["data"][$first_vid]->views*125?>px";/>
+					<?#=round($video->probability()*50)?>
+				<?php
+				}
+			}
+			?>
+		</svg>
+	</div>
+	
+	<!-- Video Blocks -->
+	<?php
+	
+###### Display Video Tree #####
+$current_vids = [$first_vid];
+$shown_vids = [];
+while($current_vids != []) {
+	#echo(implode(", ", $current_vids) . "<br/>");
+	echo("<div class='row flex-row flex-nowrap justify-content-center video-row'>");
+	$next_vids = [];
+	$just_showed_vids = [];
+	foreach($current_vids as $vid_id) {
+		
+		$video = $GLOBALS["data"][$vid_id];
+		
+		
+		##### Whether to Display This Video Now #####
+		
+		// Check if it's been displayed before
+		if(in_array($vid_id, $shown_vids)) {
+			continue;
+		}
+		
+		// Check all its parents have been displayed
+		$parents_list = $video->parents;
+		foreach($video->parents as $parent) {
+			if(in_array($parent, $shown_vids)) {
+				$parents_list = array_diff($parents_list, [$parent]);
+			}
+		}
+		if(count($parents_list) != 0){
+			array_push($next_vids,$vid_id);
+			#echo("Not displaying " . $vid_id . ": Missing parent(s) " . implode(",", $parents_list) );
+			continue;
+		}
+		
+		array_push($just_showed_vids, $vid_id);
+		
+		?>
+		
+		<div class="video <?php if($video->ending){echo("ending");}?>" id="<?=$vid_id?>" style="margin-right:<?=count($video->parents) * 50 + count($video->children) * 150?>px;magin-top:<?=random_int(10,100)?>px;">
+			<b><?=$vid_id?></b> <a href="https://youtube.com/watch?v=<?=$vid_id?>" target="__blank"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"/>
+  <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"/>
+</svg></a><br/>
+			
+			<p class="paths"><u>Paths</u>:<?=implode(", ",$video->paths)?><br/></p>
+			
+			<u>Actually</u>: <?=number_format($video->views/$GLOBALS["data"][$first_vid]->views, 3)?> /
+			<?=number_format($video->views)?> views<br/>
+			
+			<u>Predicted</u>: <?=number_format($video->probability(),3)?> /
+			<?=number_format($video->probability() * $GLOBALS["data"][$first_vid]->views)?> views<br/>
+			
+			<p class="children"><u>Children</u>:<br/><?=implode("<br/>",$video->children)?></p>
+			
+			<p class="parents"><u>Parents</u>:<br/><?=implode("<br/>",$video->parents)?></p>
+			
+			<?php if($video->ending){?>
+			<br/><h3>END</h3>
+			<?php } ?>
+			
+			<!-- Connector for path trace -->
+			<?php if(
+					count($video->children) == 2 and
+					$video->child_path[0] == "L" and
+					$video->child_path[1] == "W"
+					) { ?>
+			<div class="row">
+				<div class="offset-3 col-3" id="<?=$vid_id?>-L">Lose<br/><a href="#<?=$video->children[0]?>"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down" viewBox="0 0 16 16">
+  <path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z" stroke="red"/>
+</svg></a></div>
+				<div class="col-3" id="<?=$vid_id?>-W">Win<br/><a href="#<?=$video->children[1]?>"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down" viewBox="0 0 16 16">
+  <path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z"  stroke="green"/>
+</svg></a></div>
+			</div>
+			<?php } else if(
+					count($video->children) == 2 and
+					$video->child_path[0] == "E" and
+					$video->child_path[1] == "B"
+					) { ?>
+			<div class="row">
+				<div class="offset-3 col-3"  id="<?=$vid_id?>-E">
+				End<br/><a href="#<?=$video->children[0]?>"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down" viewBox="0 0 16 16">
+  <path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z" stroke="grey"/>
+</svg></a>
+				</div>
+				<div class="col-3"  id="<?=$vid_id?>-B">
+				Billion!<br/><a href="#<?=$video->children[1]?>"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down" viewBox="0 0 16 16">
+  <path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z" stroke="grey"/>
+</svg></a>
+				</div>
+			</div>
+			<?php } else if(
+					count($video->children) == 3 and
+					$video->child_path[0] == "R" and
+					$video->child_path[1] == "P" and
+					$video->child_path[2] == "S"
+					) { ?>
+			<div class="row">
+				<div class="col-4"  id="<?=$vid_id?>-R">
+				Rock<br/><a href="#<?=$video->children[0]?>"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down" viewBox="0 0 16 16">
+  <path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z" stroke="grey"/>
+</svg></a>
+				</div>
+				<div class="col-4"  id="<?=$vid_id?>-P">
+				Paper<br/><a href="#<?=$video->children[1]?>"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down" viewBox="0 0 16 16">
+  <path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z" stroke="grey"/>
+</svg></a>
+				</div>
+				<div class="col-4"  id="<?=$vid_id?>-S">
+				Scissors<br/><a href="#<?=$video->children[2]?>"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down" viewBox="0 0 16 16">
+  <path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z" stroke="grey"/>
+</svg></a>
+				</div>
+			</div>
+			<?php } ?>
+		</div>
+		<?php
+		
+		// Save Children of this Video For Next Row
+		$next_vids = array_merge($next_vids, $video->children);
+	}
+	echo("</div>");
+	
+	//echo("<br/><br/><br/><br/>");
+	$current_vids = array_unique($next_vids);
+	$shown_vids = array_unique(array_merge($shown_vids, $just_showed_vids));
+}
+
+	
+	?>
+	
+	
+	
+
+
+	<script src="svgDraw.js"></script>
+	<script>
+	function connectAll() {
+		<?php
+		###### Add Arrow Connections #####
+		foreach($GLOBALS["data"] as $video_id => $video) {
+			$num_children = count($video->children);
+	
+			for($j = 0; $j < $num_children; $j ++) {
+			#foreach($video->children as $child) {
+				$child_id = $video->children[$j];
+				$child_lose_win = $video->child_path[$j];
+				if($child_lose_win == "W" or $child_lose_win == "L" or $child_lose_win == "E" or $child_lose_win == "B" or $child_lose_win == "R" or $child_lose_win == "P" or $child_lose_win == "S") {
+					$child_lose_win = "-" . $child_lose_win;
+				} else {
+					$child_lose_win = "";
+				}
+			?>
+			connectElements(
+				$("#svg1"),
+				$("#<?=$video_id . "-" . $child_id?>"),
+				$("#<?=$video_id.$child_lose_win?>"),
+				$("#<?=$child_id?>"));
+			<?php
+			}
+		}
+		?>
+	}
+	
+	function Scrolldown() {
+		var rect = document.getElementById("title").getBoundingClientRect();
+		console.log((rect.right+rect.left)/2);
+		console.log(document.body.scrollWidth);
+		window.scroll(document.body.scrollWidth*0.25,0); 
+	}
+
+	window.onload = Scrolldown;
+	
+	function toggle_family() {
+		elements = document.getElementsByClassName("children");
+		for (var i = 0; i < elements.length; i++) {
+			elements[i].style.display = elements[i].style.display == 'inline' ? 'none' : 'inline';
+		}
+		elements = document.getElementsByClassName("parents");
+		for (var i = 0; i < elements.length; i++) {
+			elements[i].style.display = elements[i].style.display == 'inline' ? 'none' : 'inline';
+		}
+		
+		$(document).ready(function() {
+			// reset svg each time 
+			$("#svg1").attr("height", "0");
+			$("#svg1").attr("width", "0");
+			connectAll();
+		});
+
+		$(window).resize(function () {
+			// reset svg each time 
+			$("#svg1").attr("height", "0");
+			$("#svg1").attr("width", "0");
+			connectAll();
+		});
+	}
+	function toggle_paths() {
+		elements = document.getElementsByClassName("paths");
+		for (var i = 0; i < elements.length; i++) {
+			elements[i].style.display = elements[i].style.display == 'inline' ? 'none' : 'inline';
+		}
+		
+		$(document).ready(function() {
+			// reset svg each time 
+			$("#svg1").attr("height", "0");
+			$("#svg1").attr("width", "0");
+			connectAll();
+		});
+
+		$(window).resize(function () {
+			// reset svg each time 
+			$("#svg1").attr("height", "0");
+			$("#svg1").attr("width", "0");
+			connectAll();
+		});
+	}
+	
+	</script>
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+	
+	<style>
+	
+	#svgContainer { 
+		z-index: -10;
+		position:absolute;
+		/*background-color:white;*/
+		opacity: 0.4;
+	}
+
+	div{ opacity: 1; }
+	
+	.video {
+		width:320px;
+		margin:50px;
+		padding:10px;
+		font-size:16px;
+		margin-right:200px;
+	}
+	
+	body {
+		width:200%;
+		
+	}
+	
+	.ending {
+		border-style:solid;
+	}
+	
+	.paths {
+		display:none;
+	}
+	
+	.children, .parents {
+		display:none;
+	}
+	
+	.video-row {
+		margin-bottom:150px;
+	}
+	
+	</style>
+	
+</body>
+<!-- https://www.anychart.com/blog/2020/07/22/network-graph-javascript/ -->
---
+++ video_data.php
@@ -0,0 +1,260 @@
+<?php
+
+class video {
+	public $paths = [];
+	public $children;
+	#public $child_path;
+	#public $child_prob;
+	#public $probabity = 0;
+	public $views = 1;
+	public $parents = [];
+	
+	
+	function __construct(
+		$children = [],
+		public $child_path = ["L", "W"],
+		public $child_prob = [2/3, 1/3],
+		public $ending = False
+	) {
+		$this->children = $children;
+		$this->child_path = $child_path;
+		#$this->child_prob = $child_prob;
+		$this->ending = $ending; 
+	}
+	
+	function set_views($views = 1) {
+		$this->views = $views;
+	}
+	
+	function add_path($new_paths) {
+		$this->paths=array_merge($this->paths, $new_paths);
+	}
+	
+	function add_parent($new_parent) {
+		array_push($this->parents, $new_parent);
+		$this->parents = array_unique($this->parents);
+	}
+	
+	function probability() {
+		$total_prob = 0;
+		foreach($this->paths as $path) {
+			$L_count = substr_count($path, 'L'); // Lose
+			$W_count = substr_count($path, 'W'); // Win
+			$R_count = substr_count($path, 'R'); // Rock
+			$P_count = substr_count($path, 'P'); // Paper
+			$S_count = substr_count($path, 'S'); // Scissors
+			$E_count = substr_count($path, 'E'); // End
+			$B_count = substr_count($path, 'B'); // To A Billion
+			$total_prob += 
+				(2/3)**$L_count *
+				(1/3)**$W_count *
+				(1/3)**$R_count *
+				(1/3)**$P_count *
+				(1/3)**$S_count *
+				(1/2)**$E_count *
+				(1/2)**$B_count;
+		}
+		return($total_prob);
+	}
+	
+}
+# CHILDREN: [LOSE, WIN]
+$GLOBALS["data"] = [];
+$GLOBALS["data"]["PmWQmZXYd74"] = new video(
+	["CPb168NUwGc", "Ul8r0Thgx44"]);
+$GLOBALS["data"]["PmWQmZXYd74"]->add_path(["-"]);
+$GLOBALS["data"]["CPb168NUwGc"] = new video(
+	["jDQqv3zkbIQ", "RVLUX6BUEJI"]);
+$GLOBALS["data"]["jDQqv3zkbIQ"] = new video(
+	["HXtheRKAkIw", "RVLUX6BUEJI"]);
+$GLOBALS["data"]["HXtheRKAkIw"] = new video(
+	["3qoxLsQ9464","b41_jrE8jFw"]);
+$GLOBALS["data"]["3qoxLsQ9464"] = new video(
+	["xjo-L59q8K4","b41_jrE8jFw"]);
+$GLOBALS["data"]["xjo-L59q8K4"] = new video(
+	["dzK444eg53c","b41_jrE8jFw"]);
+$GLOBALS["data"]["dzK444eg53c"] = new video(
+	["83hQScodfDA","LLZJ-U1UB5M"]);
+$GLOBALS["data"]["LLZJ-U1UB5M"] = new video(
+	[], ending:True); // END
+$GLOBALS["data"]["83hQScodfDA"] = new video(
+	["TFlsl2ZkBlI","j-jqX7AdQT8"]);
+$GLOBALS["data"]["TFlsl2ZkBlI"] = new video(
+	["4ojQK570hDA","j-jqX7AdQT8"]);
+$GLOBALS["data"]["4ojQK570hDA"] = new video(
+	["WQ9wBn2Qk14","j-jqX7AdQT8"]);
+$GLOBALS["data"]["WQ9wBn2Qk14"] = new video(
+	["hT-25A8LFAE","j-jqX7AdQT8"]);
+$GLOBALS["data"]["hT-25A8LFAE"] = new video(
+	["e8zbuI-qJX4","j-jqX7AdQT8"]);
+$GLOBALS["data"]["e8zbuI-qJX4"] = new video(
+	["xCb7UVssqlY","j-jqX7AdQT8"]);
+$GLOBALS["data"]["xCb7UVssqlY"] = new video(
+	["54ZevZGGXZw","j-jqX7AdQT8"]);
+$GLOBALS["data"]["54ZevZGGXZw"] = new video(
+	["d84UbmiyBOs","j-jqX7AdQT8"]);
+$GLOBALS["data"]["d84UbmiyBOs"] = new video(
+	["X9jKHujmt1M","j-jqX7AdQT8"]);
+$GLOBALS["data"]["X9jKHujmt1M"] = new video(
+	["SeX6WzVRZ4Y","j-jqX7AdQT8"]);
+$GLOBALS["data"]["SeX6WzVRZ4Y"] = new video(
+	["j8fHcBHeKwk","j-jqX7AdQT8"]);
+$GLOBALS["data"]["j-jqX7AdQT8"] = new video(
+	["LvcxrEP2U-o", "oOufgnObuhQ"]);
+$GLOBALS["data"]["LvcxrEP2U-o"] = new video(
+	["dU22iL1ZsWQ"], child_path:["-"]);
+$GLOBALS["data"]["dU22iL1ZsWQ"] = new video(
+	[], ending: True); // END
+
+
+
+$GLOBALS["data"]["j8fHcBHeKwk"] = new video(
+	["VtWv7m270kY","b6_QOYNf73g"]);
+$GLOBALS["data"]["VtWv7m270kY"] = new video(
+	["LSHMwceP0X8","b6_QOYNf73g"]);
+$GLOBALS["data"]["LSHMwceP0X8"] = new video(
+	["BvL-kq_LLsI","b6_QOYNf73g"]);
+$GLOBALS["data"]["BvL-kq_LLsI"] = new video(
+	["KIcQP_OL0-0","b6_QOYNf73g"]);
+$GLOBALS["data"]["KIcQP_OL0-0"] = new video(
+	["ei5WZihztGk","b6_QOYNf73g"]);
+$GLOBALS["data"]["ei5WZihztGk"] = new video(
+	["74E6BTyhv_c","b6_QOYNf73g"]);
+$GLOBALS["data"]["74E6BTyhv_c"] = new video(
+	["YnACGEG1tTc","b6_QOYNf73g"]);
+$GLOBALS["data"]["YnACGEG1tTc"] = new video(
+	["7Jp8Xp_9v90","b6_QOYNf73g"]);
+$GLOBALS["data"]["7Jp8Xp_9v90"] = new video(
+	["ILrJDLjx6sA","b6_QOYNf73g"]);
+$GLOBALS["data"]["ILrJDLjx6sA"] = new video(
+	["sJXuw8QM0W4","b6_QOYNf73g"]);
+$GLOBALS["data"]["sJXuw8QM0W4"] = new video(
+	["Gh_preEUg74","b6_QOYNf73g"]);
+$GLOBALS["data"]["Gh_preEUg74"] = new video(
+	["YVJh73INvXk","b6_QOYNf73g"]);
+$GLOBALS["data"]["YVJh73INvXk"] = new video(
+	["9HDYmP-l_oM","b6_QOYNf73g"]);
+$GLOBALS["data"]["9HDYmP-l_oM"] = new video(
+	["ugkWE2cy370","b6_QOYNf73g"]);
+$GLOBALS["data"]["ugkWE2cy370"] = new video(
+	["F-j5y5dyPDo","b6_QOYNf73g"]);
+$GLOBALS["data"]["F-j5y5dyPDo"] = new video(
+	["tlTOyDEZGUU","b6_QOYNf73g"]);
+$GLOBALS["data"]["b6_QOYNf73g"] = new video(
+	["LvcxrEP2U-o", "oOufgnObuhQ"]);
+$GLOBALS["data"]["tlTOyDEZGUU"] = new video(
+	["GG6AZGhLCS4","wUjs_vVwh68"]);
+$GLOBALS["data"]["GG6AZGhLCS4"] = new video(
+	[], ending:True); // END
+$GLOBALS["data"]["wUjs_vVwh68"] = new video(
+	[], ending:True); // END
+$GLOBALS["data"]["RVLUX6BUEJI"] = new video(
+	["0odtRIBvjes", "t0hJIw19ChI"]);
+$GLOBALS["data"]["t0hJIw19ChI"] = new video(
+	["0odtRIBvjes", "e6zLBO0vez8"]);
+$GLOBALS["data"]["e6zLBO0vez8"] = new video(
+	["0odtRIBvjes", "WR7AbrjBZNI"]);
+$GLOBALS["data"]["WR7AbrjBZNI"] = new video(
+	["0odtRIBvjes", "fMyFx3bFW-s"]);
+$GLOBALS["data"]["fMyFx3bFW-s"] = new video(
+	["0odtRIBvjes", "hi4166mPpmA"]);
+$GLOBALS["data"]["hi4166mPpmA"] = new video(
+	["7VmxQumJAL4", "SxWZDOgaIog"]);
+$GLOBALS["data"]["SxWZDOgaIog"] = new video(
+	["7VmxQumJAL4", "-D_g1k0IzTQ"]);
+$GLOBALS["data"]["-D_g1k0IzTQ"] = new video(
+	["7VmxQumJAL4", "AnsaswKGPHk"]);
+$GLOBALS["data"]["AnsaswKGPHk"] = new video(
+	["7VmxQumJAL4", "8UKflLZq61E"]);
+$GLOBALS["data"]["8UKflLZq61E"] = new video(
+	["7VmxQumJAL4", "tAcIxmJOA9o"]);
+$GLOBALS["data"]["tAcIxmJOA9o"] = new video(
+	["QxC-EQAsTuM", "yQKjsA90kpc","_WKzx6tClQw"], child_path:["R","P","S"]);
+$GLOBALS["data"]["QxC-EQAsTuM"] = new video(
+	["LvcxrEP2U-o", "b41_jrE8jFw"]);
+$GLOBALS["data"]["_WKzx6tClQw"] = new video(
+	["LvcxrEP2U-o", "b41_jrE8jFw"]);
+$GLOBALS["data"]["yQKjsA90kpc"] = new video(
+	["s3rUNS68AKs","K1kVsxsnYyc"], child_path:["E", "B"]);
+$GLOBALS["data"]["7VmxQumJAL4"] = new video(
+	["LvcxrEP2U-o","b41_jrE8jFw"]);
+$GLOBALS["data"]["Ul8r0Thgx44"] = new video(
+	["fWOtjGJvlGI", "_mrAeT9kpPM"]);
+$GLOBALS["data"]["fWOtjGJvlGI"] = new video(
+	["0odtRIBvjes","RVLUX6BUEJI"]);
+$GLOBALS["data"]["0odtRIBvjes"] = new video(
+	["LvcxrEP2U-o","b41_jrE8jFw"]);
+$GLOBALS["data"]["b41_jrE8jFw"] = new video(
+	["LvcxrEP2U-o","oOufgnObuhQ"]);
+$GLOBALS["data"]["oOufgnObuhQ"] = new video(
+	["LvcxrEP2U-o","N7UCPssq-X8"]);
+$GLOBALS["data"]["N7UCPssq-X8"] = new video(
+	["LvcxrEP2U-o","FlwMxN9-mec"]);
+$GLOBALS["data"]["FlwMxN9-mec"] = new video(
+	["LvcxrEP2U-o","ghJAsm9W3k0"]);
+$GLOBALS["data"]["ghJAsm9W3k0"] = new video(
+	["LvcxrEP2U-o","55nbeaYL7hQ"]);
+$GLOBALS["data"]["55nbeaYL7hQ"] = new video(
+	["LvcxrEP2U-o","dB8-XaRclhk"]);
+$GLOBALS["data"]["dB8-XaRclhk"] = new video(
+	["LvcxrEP2U-o","ddWvzSxz4AA"]);
+$GLOBALS["data"]["ddWvzSxz4AA"] = new video(
+	["LvcxrEP2U-o","0xFOAtGBdUg"]);
+$GLOBALS["data"]["0xFOAtGBdUg"] = new video(
+	["LvcxrEP2U-o","HSdwcDFDyQY"]);
+$GLOBALS["data"]["HSdwcDFDyQY"] = new video(
+	["LvcxrEP2U-o","Q5kgEN3rb_c"]);
+$GLOBALS["data"]["Q5kgEN3rb_c"] = new video(
+	["LvcxrEP2U-o","pteggMrRnk4"]);
+$GLOBALS["data"]["pteggMrRnk4"] = new video(
+	["D8iP2qINaSE","hhDh6_RD7tU","87zN8iWo5pU"],child_path:["R","P","S"]);
+$GLOBALS["data"]["D8iP2qINaSE"] = new video(
+	["dU22iL1ZsWQ"], child_path:["-"]);
+$GLOBALS["data"]["hhDh6_RD7tU"] = new video(
+	["dU22iL1ZsWQ"], child_path:["-"]);
+$GLOBALS["data"]["87zN8iWo5pU"] = new video(
+	["s3rUNS68AKs","K1kVsxsnYyc"], child_path:["E","B"]);
+$GLOBALS["data"]["s3rUNS68AKs"] = new video(
+	[], ending:True); // END
+$GLOBALS["data"]["K1kVsxsnYyc"] = new video(
+	[], ending:True); // BILLION
+$GLOBALS["data"]["dU22iL1ZsWQ"] = new video(
+	[], ending:True); // BILLION
+$GLOBALS["data"]["_mrAeT9kpPM"] = new video(
+	["r9-jSTCiHd0","hoaLwPc571E"]);
+$GLOBALS["data"]["r9-jSTCiHd0"] = new video(
+	["0odtRIBvjes","RVLUX6BUEJI"]);
+$GLOBALS["data"]["hoaLwPc571E"] = new video(
+	["qYaLoO40kjM","z8zjvT8Qx8U"]);
+$GLOBALS["data"]["qYaLoO40kjM"] = new video(
+	[]);
+$GLOBALS["data"]["z8zjvT8Qx8U"] = new video(
+	["qYaLoO40kjM","uHoYnV9JX4w"]);
+$GLOBALS["data"]["uHoYnV9JX4w"] = new video(
+	["qYaLoO40kjM","jRDLtKUsq8U"]);
+$GLOBALS["data"]["jRDLtKUsq8U"] = new video(
+	["qYaLoO40kjM","RXy0Kc1Cl9s"]);
+$GLOBALS["data"]["RXy0Kc1Cl9s"] = new video(
+	["yCwdjfzxI4I","v3oXQrWu-PA"]);
+$GLOBALS["data"]["v3oXQrWu-PA"] = new video(
+	["yCwdjfzxI4I","bN5M3caw6b8"]);
+$GLOBALS["data"]["bN5M3caw6b8"] = new video(
+	["yCwdjfzxI4I","DDmnplXv6pY"]);
+$GLOBALS["data"]["DDmnplXv6pY"] = new video(
+	["yCwdjfzxI4I","6auFOPOuHuE"]);
+$GLOBALS["data"]["6auFOPOuHuE"] = new video(
+	["yCwdjfzxI4I","0M39bd9euEI"]);
+$GLOBALS["data"]["0M39bd9euEI"] = new video(
+	["E3pdr5hNBe4","YohvsF9mF3g","AGL2OMZzn2g"], child_path:["R","P","S"]);
+$GLOBALS["data"]["E3pdr5hNBe4"] = new video(
+	["s3rUNS68AKs","K1kVsxsnYyc"], child_path:["E","B"]);
+$GLOBALS["data"]["YohvsF9mF3g"] = new video(
+	["0odtRIBvjes","RVLUX6BUEJI"]);
+$GLOBALS["data"]["AGL2OMZzn2g"] = new video(
+	["0odtRIBvjes","RVLUX6BUEJI"]);
+
+
+$GLOBALS["data"]["yCwdjfzxI4I"] = new video(
+	["0odtRIBvjes","RVLUX6BUEJI"]);
+
+?>
