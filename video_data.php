4c5e94e01b6555c8743d2a2d17955fc65925d2d5
athuler <athuler@uchicago.edu>
2023-10-27 13:57:03 -0500

    Update video_data.php

--- video_data.php
+++ video_data.php
@@ -9,13 +9,11 @@ class video {
 	public $views = 1;
 	public $parents = [];
 	public $child_path;
-	public $child_prob;
 	public $ending;
 	
 	function __construct(
 		$children = [],
 		$child_path = ["L", "W"],
-		$child_prob = [2/3, 1/3],
 		$ending = False
 	) {
 		$this->children = $children;
@@ -77,7 +75,7 @@ $GLOBALS["data"]["xjo-L59q8K4"] = new video(
 	["dzK444eg53c","b41_jrE8jFw"]);
 $GLOBALS["data"]["dzK444eg53c"] = new video(
 	["83hQScodfDA","LLZJ-U1UB5M"]);
-$GLOBALS["data"]["LLZJ-U1UB5M"] = new video(ending:True); // END
+$GLOBALS["data"]["LLZJ-U1UB5M"] = new video(ending: True); // END
 $GLOBALS["data"]["83hQScodfDA"] = new video(
 	["TFlsl2ZkBlI","j-jqX7AdQT8"]);
 $GLOBALS["data"]["TFlsl2ZkBlI"] = new video(
