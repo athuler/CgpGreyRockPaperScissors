906c0f4a698a9ff3aeed8b8d23dba8ec1bf968e5
athuler <athuler@uchicago.edu>
2023-10-27 14:05:30 -0500

    Update index.php

--- index.php
+++ index.php
@@ -82,7 +82,7 @@ foreach($all_ids as $id_list) {
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 </head>
 <!--<body class="text-center container-fluid overflow-x-scroll">-->
 <body class="text-center overflow-x-auto">
