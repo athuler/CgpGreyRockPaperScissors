6276f1b67b77e7920a75930972f9c153d85094d6
Andrei Thüler <22741115+athuler@users.noreply.github.com>
2025-12-14 12:50:36 -0500

    Disable overscroll/bouncing

--- index-cytoscape.php
+++ index-cytoscape.php
@@ -105,7 +105,7 @@ foreach($GLOBALS["data"] as $vid){
 <head>
 	<title>CGP Grey: Rock Paper Scissors - A Statistical Analysis</title>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, viewport-fit=cover">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
 	<link rel="stylesheet" href="cytoscape-styles.css">
